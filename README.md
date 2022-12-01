0017bce4c859c36e553e7a1447ec6395b8d8037f
rigrergl <rigrergl@gmail>
2022-11-30 21:39:21 -0600

    changed server to accept multiple connections (testing yet to be done)

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ## Assumption
 - Inputs are two numbers separated by an operand Ex: "1 + 1" "1 - 1" "4 / 2"
+
+## Our Protocol:
+"end" = terminate connection
--- TCPServer.java
+++ TCPServer.java
@@ -2,26 +2,51 @@ import java.io.*;
 import java.net.*;
 
 class TCPServer {
+  private static final String TERMINATE_MESSAGE = "end";
 
   public static void main(String argv[]) throws Exception {
-    String clientSentence;
-    String capitalizedSentence;
-
     ServerSocket welcomeSocket = new ServerSocket(6789);
 
     while (true) {
-
       Socket connectionSocket = welcomeSocket.accept();
-
-      BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-
-      DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-
-      clientSentence = inFromClient.readLine();
-
-      capitalizedSentence = clientSentence.toUpperCase() + '\n';
-
-      outToClient.writeBytes(capitalizedSentence);
+      Runnable connectionHandler = new ConnectionHandler(connectionSocket);
+      new Thread(connectionHandler).start();
     }
+  }
+
+  private static class ConnectionHandler implements Runnable {
+    private final Socket clientSocket;
+
+    public ConnectionHandler(Socket clientSocket) {
+      this.clientSocket = clientSocket;
+    }
+
+    @Override
+    public void run() {
+      BufferedReader inFromClient;
+      DataOutputStream outToClient;
+
+      try {
+        inFromClient = new BufferedReader(new InputStreamReader(this.clientSocket.getInputStream()));
+        outToClient = new DataOutputStream(this.clientSocket.getOutputStream())
+      } catch (Exception e) {
+        System.out.println("Exception occurred in server: " + e.getMessage());
+      }
+
+      String clientExpression;
+      String expressionResult;
+      while (true) {
+        clientExpression = inFromClient.readLine();
+
+        if (clientExpression.equals(TERMINATE_MESSAGE)) {
+          this.clientSocket.close();
+          break;
+        }
+
+        expressionResult = clientExpression.toUpperCase() + '\n'; // TODO: change to actual result
+        outToClient.writeBytes(expressionResult);
+      }
+    }
+
   }
 }
