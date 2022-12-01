cc51cc230ac739736a780b6cdffd10f9af654050
rigrergl <rigrergl@gmail>
2022-12-01 17:20:21 -0600

    run client and run server

--- automate.py
+++ automate.py
@@ -2,7 +2,19 @@ import subprocess
 def compile_java(java_file):
     # cmd = '/path/to/javac/javac ' + java_file 
     cmd = 'C:/\"Program Files\"/Java/jdk-19/bin/javac.exe ' + java_file 
+    proc = subprocess.Popen(cmd, shell=True, creationflags=subprocess.CREATE_NEW_CONSOLE)
+
+def run_server():
+    cmd = 'java ' + "TCPServer"
     proc = subprocess.Popen(cmd, shell=True)
+
+
+def run_client(client_name): 
+    cmd = 'java TCPClient ' + client_name
+    proc = subprocess.Popen(cmd, shell=False)
 
 compile_java("TCPServer.java")
 compile_java("TCPClient.java")
+
+run_server()
+run_client("joe")
