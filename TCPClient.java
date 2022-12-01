6f179b0906381dbe4b620a7f9c598170cc6d8436
hannahhlg <90477692+hannahhlg@users.noreply.github.com>
2022-12-01 16:56:16 -0600

    Update TCPClient.java

--- TCPClient.java
+++ TCPClient.java
@@ -4,35 +4,35 @@ import java.net.*;
 import java.util.function.IntPredicate;
 
 class TCPClient {
-  private static final String TERMINATE_MESSAGE = "end";
+  private static final String TERMINATE_MESSAGE = "end"; //specifying message to end the connection
 
 
   public static void main(String[] args) throws Exception {
 
-    if(args.length != 1){
+    if(args.length != 1){ // if the client name isn't included when creating request, send error
         System.out.println("Error. Please include client name when creating request");
     }
 
-
-
-    Socket clientSocket = new Socket("127.0.0.1", 6789);
+    Socket clientSocket = new Socket("127.0.0.1", 6789); //creating client scoket
     
-    
-    DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-    BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-    BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+    DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream()); //creating way to send to server
+    BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); //creating way to receive from server
+    BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in)); //creating way to receive from user
 
+    String clientName = args[0]; //storing client name from command line
+    outToServer.writeBytes(clientName + '\n'); //sending name to server
 
-
-    String clientName = args[0];
-    outToServer.writeBytes(clientName + '\n');
-
-
-    System.out.print("\nENTER THE MATH EXPRESSION >>> ");
-
-    String expression = inFromUser.readLine();
+    //getting expression from user
+    System.out.print("\nENTER THE MATH EXPRESSION >>> "); 
+    String expression = inFromUser.readLine(); 
     String result;
 
+    /*until the user decides to terminate the connection: repeat this: 
+        sending to server the expression taken from user
+        getting the result back form the server
+        printing result
+        requesting user to give another expression
+    */
     while (!expression.equals(TERMINATE_MESSAGE)) {
       outToServer.writeBytes(expression + '\n');
       result = inFromServer.readLine();
@@ -42,7 +42,10 @@ class TCPClient {
       expression = inFromUser.readLine();
     }
 
-    outToServer.writeBytes(TERMINATE_MESSAGE);
+    //once the user gives the termination message, send that to server
+    outToServer.writeBytes(TERMINATE_MESSAGE); 
+
+    //close the socket
     System.out.println("Connection Close Thank you for using our calculator");
     clientSocket.close();
   }
